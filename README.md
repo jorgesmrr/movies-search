0100608b73ef346b805655d5f0dfeb466505db4d
Jorge Moreira <jorge.smrr@gmail.com>
2021-05-03 21:40:48 -0300

    Setup project with Next.js and TypeScript

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a project to search and discover movies and TV shows.
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Movies and TV shows</title>
+        <meta
+          name="description"
+          content="Search and discover movies and TV shows"
+        />
+      </Head>
+    </div>
+  );
+}
