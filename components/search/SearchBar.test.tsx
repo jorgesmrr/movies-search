e3058758cd62fa5cf8c453e718a4cbfbe24443c0
Jorge Moreira <jorge.smrr@gmail.com>
2021-05-04 21:07:39 -0300

    Add search submit callback and clear button

--- components/search/SearchBar.test.tsx
+++ components/search/SearchBar.test.tsx
@@ -1,14 +1,51 @@
 import React from "react";
 import SearchBar from "./SearchBar";
-import { render, screen } from "@testing-library/react";
-
-type A = number;
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("component renders", () => {
   test("should be empty", () => {
     render(<SearchBar />);
 
     const searchInput = screen.getByTestId("search-input");
+
+    expect(searchInput.textContent).toBe("");
+  });
+});
+
+describe("user types in", () => {
+  test("should call the submit function on Enter key press", () => {
+    const onSubmit = jest.fn();
+
+    render(<SearchBar onSubmit={onSubmit} />);
+
+    const searchInput = screen.getByTestId("search-input");
+
+    userEvent.type(searchInput, "{enter}");
+    expect(onSubmit).toHaveBeenCalled();
+  });
+
+  test("should call the submit function with the typed text", () => {
+    const onSubmit = jest.fn();
+
+    render(<SearchBar onSubmit={onSubmit} />);
+
+    const searchInput = screen.getByTestId("search-input");
+
+    const searchText = "Some movie name here";
+    userEvent.type(searchInput, `${searchText}{enter}`);
+    expect(onSubmit).toHaveBeenCalledWith(searchText);
+  });
+
+  test("should be empty after 'clear' button click", () => {
+    render(<SearchBar />);
+
+    const searchInput = screen.getByTestId("search-input");
+    const clearButton = screen.getByTestId("search-clear");
+
+    const searchText = "Some movie name here";
+    userEvent.type(searchInput, searchText);
+    fireEvent.click(clearButton);
 
     expect(searchInput.textContent).toBe("");
   });
--- components/search/SearchBar.tsx
+++ components/search/SearchBar.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SearchBar: React.FC = () => {
-  return <input type="text" data-testid="search-input" />;
+export interface SearchBarProps {
+  onSubmit?: (text: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSubmit }) => {
+  const [text, setText] = useState("");
+
+  const onChange: React.ChangeEventHandler<HTMLInputElement> = (event) =>
+    setText(event.target.value);
+
+  const onKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (event) => {
+    if (event.key === "Enter") {
+      onSubmit?.(text);
+    }
+  };
+
+  const onClearClick = () => setText("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        data-testid="search-input"
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+      />
+      <div data-testid="search-clear" onClick={onClearClick} />
+    </div>
+  );
 };
 
 export default SearchBar;
