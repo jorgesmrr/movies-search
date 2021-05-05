ee8f0c147714a3a9e4c4ad371644505d88716361
Jorge Moreira <jorge.smrr@gmail.com>
2021-05-05 03:25:53 -0300

    Load movies in list

--- components/movie/movie-list/MovieList.test.tsx
+++ components/movie/movie-list/MovieList.test.tsx
@@ -1,19 +1,44 @@
-import { render, screen } from "@testing-library/react";
+import { act, render, screen, waitFor } from "@testing-library/react";
 import MovieList from "./MovieList";
 import axios from "axios";
 
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+const fakeData = {
+  data: [
+    { id: 1, title: "Foo" },
+    { id: 2, title: "Bar" },
+  ],
+};
+
 describe("component renders", () => {
-  beforeEach(() => render(<MovieList />));
+  beforeEach(() => {
+    render(<MovieList />);
+  });
 
   test("should show spinner", () => {
     expect(screen.getByTestId("movie-list__spinner")).toBeTruthy();
   });
 
   test("should start fetching movies", () => {
-    expect(axios.get).toHaveBeenCalled();
+    expect(mockedAxios.get).toHaveBeenCalled();
+  });
+});
+
+describe("loads movies", () => {
+  beforeEach(async () => {
+    mockedAxios.get.mockResolvedValueOnce(fakeData);
+
+    render(<MovieList />);
+    await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
   });
 
-  test("should show movies list", () => {
-    expect(screen.findByTestId("movie-list__movies")).toBeTruthy();
+  test("should show movies list", async () => {
+    expect(
+      await screen.findByRole("list", { name: "movies list" })
+    ).toBeTruthy();
+
+    expect(screen.getByRole("listitem", { name: fakeData.data[0].title }));
   });
 });
--- components/movie/movie-list/MovieList.tsx
+++ components/movie/movie-list/MovieList.tsx
@@ -4,12 +4,25 @@ import axios from "axios";
 const MovieList: React.FC = () => {
   const [movies, setMovies] = useState();
 
+  const fetchMovies = async () => {
+    const response = await axios.get("");
+    setMovies(response.data);
+  };
+
   useEffect(() => {
-    axios.get("").then((response) => setMovies(response.data));
+    fetchMovies();
   }, []);
 
+  const renderedMovies = movies
+    ? movies.map((movie) => (
+        <li key={movie.id} aria-label={movie.title}>
+          {movie.title}
+        </li>
+      ))
+    : null;
+
   return movies ? (
-    <ul data-testid="movie-list__movies"></ul>
+    <ul aria-label="movies list">{renderedMovies}</ul>
   ) : (
     <div data-testid="movie-list__spinner" />
   );
