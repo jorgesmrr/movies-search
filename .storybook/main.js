a8850d71dcd4a5e1db0e59236c0a898ece862db2
Jorge Moreira <jorge.smrr@gmail.com>
2021-05-07 20:31:34 -0300

    Remove @compiled/react

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,17 +4,4 @@ module.exports = {
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(js|ts|tsx)$/,
-      exclude: /node_modules/,
-      use: [
-        {
-          loader: "@compiled/webpack-loader",
-        },
-      ],
-    });
-
-    return config;
-  },
 };
