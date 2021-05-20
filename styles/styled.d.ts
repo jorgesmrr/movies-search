fc89b0b07b8b06dd5f4b986e682b52b7c93b3765
Jorge Moreira <jorge.smrr@gmail.com>
2021-05-20 19:15:24 -0300

    Fix theme declaration

--- styles/styled.d.ts
+++ styles/styled.d.ts
@@ -1,6 +1,5 @@
 import "styled-components";
 
-// and extend them!
 declare module "styled-components" {
   export interface DefaultTheme {
     radius: string;
@@ -15,11 +14,26 @@ declare module "styled-components" {
         darker: string;
         darkest: string;
       };
+      accent: {
+        lightest: string;
+        lighter: string;
+        light: string;
+        default: string;
+        dark: string;
+        darker: string;
+        darkest: string;
+      };
     };
     shadow: string[];
     fontSize: {
       mobile: number[];
       desktop: number[];
     };
+    breakpoints: {
+      sm: string;
+      md: string;
+      lg: string;
+    };
+    pageWidth: number;
   }
 }
