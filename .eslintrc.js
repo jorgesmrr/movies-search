8ece7b12577d020d26517593e21dc1550bb61acd
Jorge Moreira <jorge.smrr@gmail.com>
2021-05-26 22:15:23 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,6 @@ module.exports = exports = {
   rules: {
     "react/prop-types": "off",
     "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/no-unused-vars": "error",
   },
   settings: {
     react: {
