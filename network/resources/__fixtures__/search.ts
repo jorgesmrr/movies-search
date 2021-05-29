b05f09107c03b2fc46843a652eeae5cc846598ca
Jorge Moreira <jorge.smrr@gmail.com>
2021-05-29 17:22:24 -0300

    Update fixtures

--- network/resources/__fixtures__/search.ts
+++ network/resources/__fixtures__/search.ts
@@ -1,24 +1,22 @@
 export const getSearchMoviesFixture = () => ({
   page: 1,
-  results: [
-    {
-      poster_path: "/cezWGskPY5x7GaglTTRN4Fugfb8.jpg",
-      adult: false,
-      overview:
-        "When an unexpected enemy emerges and threatens global safety and security, Nick Fury, director of the international peacekeeping agency known as S.H.I.E.L.D., finds himself in need of a team to pull the world back from the brink of disaster. Spanning the globe, a daring recruitment effort begins!",
-      release_date: "2012-04-25",
-      genre_ids: [878, 28, 12],
-      id: 24428,
-      original_title: "The Avengers",
-      original_language: "en",
-      title: "The Avengers",
-      backdrop_path: "/hbn46fQaRmlpBuUrEiFqv0GDL6Y.jpg",
-      popularity: 7.353212,
-      vote_count: 8503,
-      video: false,
-      vote_average: 7.33,
-    },
-  ],
+  results: [...new Array(20)].map(() => ({
+    adult: false,
+    backdrop_path: "/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
+    genre_ids: [28, 12, 14, 878],
+    id: 299536,
+    original_language: "en",
+    original_title: "Avengers: Infinity War",
+    overview:
+      "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
+    poster_path: "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+    release_date: "2018-04-25",
+    title: "Avengers: Infinity War",
+    video: false,
+    vote_average: 8.3,
+    vote_count: 6937,
+    popularity: 358.799,
+  })),
   total_results: 14,
   total_pages: 1,
 });
