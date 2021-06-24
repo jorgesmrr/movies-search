aa758a6c3c782d0e50d2de977b3e24a95061ccf3
Jorge Moreira <jorge.smrr@gmail.com>
2021-06-23 23:57:17 -0300

    Improve Button styles

--- components/button/Button.styles.ts
+++ components/button/Button.styles.ts
@@ -2,19 +2,32 @@ import styled from "styled-components";
 import { transition } from "../style/style";
 
 export const Button = styled.button`
+  display: flex;
+  align-items: center;
   color: ${({ theme }) => theme.color.white};
   background-color: ${({ theme }) => theme.color.neutral.darker};
   border-radius: ${({ theme }) => theme.dimensions.radius};
   box-shadow: ${({ theme }) => theme.shadow[1]};
   border: none;
-  cursor: pointer;
-  ${transition("background-color")}
+  padding: 0.25rem 0.5rem;
+  appearance: none;
+  font-size: 1em;
 
-  &:hover {
+  &[disabled] {
+    color: ${({ theme }) => theme.color.neutral.default};
+    background-color: ${({ theme }) => theme.color.neutral.darkest};
+  }
+
+  &:not([disabled]) {
+    cursor: pointer;
+    ${transition("background-color")}
+  }
+
+  &:hover:not([disabled]) {
     background-color: ${({ theme }) => theme.color.accent.dark};
   }
 
-  &:active {
+  &:active:not([disabled]) {
     background-color: ${({ theme }) => theme.color.accent.darker};
   }
 `;
