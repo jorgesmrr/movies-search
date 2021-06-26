701f613c95af8f9aab5ba16f8551654a9a1052f2
Jorge Moreira <jorge.smrr@gmail.com>
2021-06-26 00:51:55 -0300

    Update movies grid layout

--- components/movie/movie-list/MovieListGrid.styles.ts
+++ components/movie/movie-list/MovieListGrid.styles.ts
@@ -4,15 +4,9 @@ import { ListNone } from "../../style/style";
 export const Grid = styled(ListNone)<{ columns: number }>`
   display: grid;
   gap: 1rem;
-  grid-template-columns: repeat(
-    auto-fill,
-    minmax(min(${({ columns }) => 60 / columns}rem, 100%), 1fr)
-  );
+  grid-template-columns: repeat(auto-fill, minmax(min(10rem, 100%), 1fr));
 
   @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
-    grid-template-columns: repeat(
-      auto-fill,
-      minmax(min(${({ columns }) => 70 / columns}rem, 100%), 1fr)
-    );
+    grid-template-columns: repeat(auto-fill, minmax(min(14rem, 100%), 1fr));
   }
 `;
